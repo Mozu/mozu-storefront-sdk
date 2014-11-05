d48e9c4446b7db514a615f99bbbc8614d49bf3d1
james <zetlen@gmail.com>
2014-11-05 08:54:11 -0600

    windows compatibility and complete dependency list

--- maketest.js
+++ maketest.js
@@ -1,5 +1,12 @@
-require('child_process')
-  .spawn(require('path').resolve('node_modules','.bin','mocha'), [], { stdio: 'inherit' })
-  .on('close', function() {
-    process.exit(0);
-  });
+var childProcess = require('child_process'),
+  mochaExecutablePath = require('path').resolve('node_modules','.bin','mocha'),
+  testProcess;
+if (/^win/.test(process.platform)) {
+  testProcess = childProcess.spawn('cmd', ['/c', mochaExecutablePath], { stdio: 'inherit' });
+} else {
+  testProcess = childProcess.spawn(mochaExecutablePath, [], { stdio: 'inherit' });
+}
+
+testProcess.on('close', function() {
+  process.exit(0);
+});
